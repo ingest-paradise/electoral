30408f2d599ade7cef91f9da2ead6f376369aa79
Javier Barón <javibp_95@hotmail.com>
2019-04-03 08:24:17 +0200

    read tweets and print them

--- src/main/scala/org/paradise/ingest/ElectoralApp.scala
+++ src/main/scala/org/paradise/ingest/ElectoralApp.scala
@@ -5,24 +5,15 @@ import org.apache.spark.sql.SparkSession
 /**
  * @author ${user.name}
  */
-object ElectoralApp {
-
-  def main(args : Array[String]) {
-    val spark = SparkSession.builder()
-      .appName("App for predicting Spain Electoral 2019 results")
-      .master("local[*]")
-      .getOrCreate()
+object ElectoralApp extends App {
+  
+  val spark = SparkSession.builder()
+    .appName("App for predicting Spain Electoral 2019 results")
+    .master("local[*]")
+    .getOrCreate()
 
     val sc = spark.sparkContext
-
-    val col = sc.parallelize(0 to 100 by 5)
-    val smp = col.sample(true, 4)
-    val colCount = col.count
-    val smpCount = smp.count
-
-    println("orig count = " + colCount)
-    println("sampled count = " + smpCount)
-    println("test")
-  }
+    
+    Twitter.stream()
 
 }
